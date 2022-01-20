d9da3cc2777c42e3e50a01e6b323cdf43204d3df
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2022-01-19 18:10:07 -0800

    bump version

--- src/main/java/ooo/foooooooooooo/velocitydiscord/VelocityDiscord.java
+++ src/main/java/ooo/foooooooooooo/velocitydiscord/VelocityDiscord.java
@@ -20,7 +20,7 @@ import java.util.logging.Logger;
 public class VelocityDiscord {
     public static final String PluginName = "Velocity Discord Bridge";
     public static final String PluginDescription = "Velocity Discord Chat Bridge";
-    public static final String PluginVersion = "1.0.1";
+    public static final String PluginVersion = "1.0.2";
     public static final String PluginUrl = "https://github.com/fooooooooooooooo/VelocityDiscord";
 
     private final ProxyServer server;
