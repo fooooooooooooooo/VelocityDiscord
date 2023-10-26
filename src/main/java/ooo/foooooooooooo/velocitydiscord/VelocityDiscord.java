dc3f254270977604cc9a58b8f129b814b83ace8d
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2023-10-25 22:22:59 -0700

    bump version

--- src/main/java/ooo/foooooooooooo/velocitydiscord/VelocityDiscord.java
+++ src/main/java/ooo/foooooooooooo/velocitydiscord/VelocityDiscord.java
@@ -27,7 +27,7 @@ import java.util.logging.Logger;
 public class VelocityDiscord {
   public static final String PluginName = "Velocity Discord Bridge";
   public static final String PluginDescription = "Velocity Discord Chat Bridge";
-  public static final String PluginVersion = "1.8.0";
+  public static final String PluginVersion = "1.8.1";
   public static final String PluginUrl = "https://github.com/fooooooooooooooo/VelocityDiscord";
 
   public static final MinecraftChannelIdentifier YepIdentifier = MinecraftChannelIdentifier.create("velocity", "yep");
