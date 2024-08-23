d29f9c34e6643a966fd1cd5864dba9639e8bcb0d
fooooooooooooooo <34515228+fooooooooooooooo@users.noreply.github.com>
2024-08-23 12:40:41 -0700

    bump version to 1.9.0 and update dependencies

--- src/main/java/ooo/foooooooooooo/velocitydiscord/VelocityDiscord.java
+++ src/main/java/ooo/foooooooooooo/velocitydiscord/VelocityDiscord.java
@@ -35,7 +35,7 @@ import java.util.logging.Logger;
 public class VelocityDiscord {
   public static final String PluginName = "Velocity Discord Bridge";
   public static final String PluginDescription = "Velocity Discord Chat Bridge";
-  public static final String PluginVersion = "1.9.0-pre.1";
+  public static final String PluginVersion = "1.9.0";
   public static final String PluginUrl = "https://github.com/fooooooooooooooo/VelocityDiscord";
 
   public static final String YeplibId = "yeplib";
